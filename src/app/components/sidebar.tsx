48d9370f1c0d4c9785e66a1758501ba7d5f06c7e
Johnny Jiang <giovannilance94@gmail.com>
2025-01-27 21:36:43 +1030

    fix: update sidebar user link to plural form

--- src/app/components/sidebar.tsx
+++ src/app/components/sidebar.tsx
@@ -76,7 +76,7 @@ function Sidebar() {
           <SidebarLink icon={Briefcase} label="Timeline" href="/timeline" />
           <SidebarLink icon={Search} label="Search" href="/search" />
           <SidebarLink icon={Settings} label="Settings" href="/settings" />
-          <SidebarLink icon={User} label="User" href="/user" />
+          <SidebarLink icon={User} label="Users" href="/users" />
           <SidebarLink icon={Users} label="Teams" href="/teams" />
         </nav>
 

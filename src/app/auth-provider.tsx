7448c7a45e7e4abf8a756329be5d1231f7fb4e2b
Johnny Jiang <giovannilance94@gmail.com>
2025-01-30 21:10:28 +1030

    config: enable server-side rendering for AWS Amplify authentication

--- src/app/auth-provider.tsx
+++ src/app/auth-provider.tsx
@@ -4,14 +4,17 @@ import { Authenticator } from "@aws-amplify/ui-react";
 
 import { Amplify } from "aws-amplify";
 
-Amplify.configure({
-  Auth: {
-    Cognito: {
-      userPoolId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID!,
-      userPoolClientId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_CLIENT_ID!,
+Amplify.configure(
+  {
+    Auth: {
+      Cognito: {
+        userPoolId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID!,
+        userPoolClientId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_CLIENT_ID!,
+      },
     },
   },
-});
+  { ssr: true },
+);
 
 const FormFields = {
   signUp: {
